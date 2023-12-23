9bff72968c95d1ab73c0dcf29cbdd81f348b7cec
Saad <byiringirosaad@gmail.com>
2023-12-23 09:54:35 +0200

    login updated

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,43 +1,42 @@
 const Login = () => {
   return (
-    <div className="w-full h-auto flex flex-col items-center p-10">
-      <p className="text-white text-2xl font-bold mb-10">Login</p>
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center p-10">
+      <div className="mb-20 flex flex-col items-center justify-center">
+        <p className="text-white text-4xl font-bold mb-10">Login</p>
+        <p className="text-white">Welcome to Product Management Mini System</p>
+      </div>
       <form className="w-[40%] p-4 md:p-5">
-        <div className="grid gap-4 mb-4 grid-cols-2">
-          <div className="col-span-2">
-            <label
-              htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Username"
-              required
-            />
-          </div>
+        <div className="col-span-2 mb-8">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="Username"
+            required
+          />
         </div>
-        <div className="grid gap-4 mb-4 grid-cols-2">
-          <div className="col-span-2">
-            <label
-              htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="name"
-              id="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Password"
-              required
-            />
-          </div>
+        <div className="col-span-2 mb-8">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="name"
+            id="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="Password"
+            required
+          />
         </div>
         <button
           type="button"
